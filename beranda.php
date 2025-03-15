11664a3f17eeb96e16c2548dc9e46daeff274b7c
amad207 <hantakchimat@gmail.com>
2025-03-15 20:19:47 +0800

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Restoran Amad</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Selamat Datang ke Restoran Amad</h1>
+    <nav>
+        <a href="index.php">Laman Utama</a> |
+        <a href="menu.php">Menu</a> |
+        <a href="pesanan.php">Pesanan</a>
+    </nav>
+    <p>Sila pilih makanan kegemaran anda daripada menu kami.</p>
+</body>
+</html>
